43d4b0212b27229034d9027e7b974327c166f1e9
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2021-05-03 08:23:14 +0300

    implemented auto youla parser
    3fixed

--- gb_parse/spiders/autoyoula.py
+++ gb_parse/spiders/autoyoula.py
@@ -1,10 +1,30 @@
 import scrapy
+import pymongo
+import re
+from urllib.parse import urljoin, unquote
+from base64 import b64decode
 
 
 class AutoyoulaSpider(scrapy.Spider):
     name = "autoyoula"
     allowed_domains = ["auto.youla.ru"]
     start_urls = ["https://auto.youla.ru/"]
+
+    _re_pattern_dealer_check = re.compile(r"sellerLink%22%2Cnull%2C%22type")
+    _re_pattern_user = re.compile(r"youlaId%22%2C%22([a-zA-Z|\d]+)%22%2C%22avatar")
+    _re_pattern_dealer = re.compile(r"sellerLink%22%2C%22([\W|a-zA-Z|\d]+)%22%2C%22type")
+
+    _re_pattern_img = re.compile(
+        r"%2Fstatic.am%2Fautomobile_m3%2Fdocument%2F([a-zA-z|\d|\%]+).jpg"
+    )
+
+    _re_pattern_phone = re.compile(r"phone%22%2C%22([a-zA-Z|\d|%]+)%22%2C%22time")
+
+    _base_image_url = "https://static.am/automobile_m3/document/"
+
+    def __init__(self):
+        db_client = pymongo.MongoClient("mongodb://localhost:27017")
+        self.db = db_client["gb_data_mining_auto_youla_21_04_2021"]
 
     def _get_follow(self, response, selector_str, callback):
         for itm in response.css(selector_str):
@@ -15,52 +35,74 @@ class AutoyoulaSpider(scrapy.Spider):
         yield from self._get_follow(
             response,
             ".TransportMainFilters_brandsList__2tIkv .ColumnItemList_container__5gTrc .ColumnItemList_item__32nYI a.blackLink",
-            self.brand_parse,
+            self._brand_parse,
         )
 
-    def brand_parse(self, response):
+    def _brand_parse(self, response):
         yield from self._get_follow(
-            response, ".Paginator_block__2XAPy .Paginator_button__u1e7D", self.brand_parse
+            response, ".Paginator_block__2XAPy .Paginator_button__u1e7D", self._brand_parse
         )
 
         yield from self._get_follow(
             response,
             "article.SerpSnippet_snippet__3O1t2 a.SerpSnippet_name__3F7Yu.blackLink",
-            self.car_parse,
+            self._car_parse,
         )
 
-    def car_parse(self, response):
-        title = response.css(".AdvertCard_advertTitle__1S1Ak::text").extract_first()
-        images = response.css(".PhotoGallery_photoWrapper__3m7yM .PhotoGallery_photoImage__2mHGn")
-        description = response.css(".AdvertCard_descriptionInner__KnuRi::text").extract_first()
-        protected = response.css(
-            ".AdvertEquipment_equipmentSection__3YpK5 div.AdvertEquipment_equipmentItem__Jk5c4::text"
-        )
-        comfort = response.css(
-            ".AdvertEquipment_equipmentSection__3YpK5 div.AdvertEquipment_equipmentItem__Jk5c4::text"
-        )
-        ext = response.css(
-            ".AdvertEquipment_equipmentSection__3YpK5 div.AdvertEquipment_equipmentItem__Jk5c4::text"
-        )
-        media = response.css(
-            ".AdvertEquipment_equipmentSection__3YpK5 div.AdvertEquipment_equipmentItem__Jk5c4::text"
-        )
-
-        # characters_keys = response.css(".AdvertCard_specs__2FEHc .AdvertSpecs_row__ljPcX div.AdvertSpecs_label__2JHnS::text").extract()
-        # characters_values = response.css(".AdvertCard_specs__2FEHc .AdvertSpecs_row__ljPcX div.AdvertSpecs_data__xK2Qx::text").extract()
-        # characteristics = {}
-        # for i in range(len(characters_keys)):
-        #    characteristics[characters_keys[i]] = characters_values[i]
-
-        data = {
-            "url": response.url,
-            "title": title,
-            "images_urls": list(map(lambda img: img.attrib["src"], images)),
-            "description": description,
-            "protected": list(map(lambda row: row.extract(), protected)),
-            "comfort": list(map(lambda row: row.extract(), comfort)),
-            "ext": list(map(lambda row: row.extract(), ext)),
-            "media": list(map(lambda row: row.extract(), media))
-            # "characteristics": characteristics
+    def _car_parse(self, response):
+        characteristics = response.css(
+            "div.AdvertCard_specs__2FEHc .AdvertSpecs_row__ljPcX *::text"
+        ).getall()
+        car_dict = {
+            "title": response.css(".AdvertCard_advertTitle__1S1Ak::text").get(),
+            "price": float(
+                response.css("div.AdvertCard_price__3dDCr::text").get().replace("\u2009", "")
+            ),
+            "characteristics": {
+                characteristics[i]: characteristics[i + 1]
+                for i in range(0, len(characteristics), 2)
+            },
+            "description": response.css(
+                "div.AdvertCard_descriptionWrap__17EU3 .AdvertCard_descriptionInner__KnuRi::text"
+            ).get(),
         }
-        print(data)
+        car_dict.update(self._get_owner_info(response))
+        self._save(car_dict)
+
+    def _get_owner_info(self, response):
+        marker = "window.transitState = decodeURIComponent"
+        for script in response.css("script"):
+            try:
+                if marker in script.css("::text").extract_first():
+                    car_dict_update = {
+                        "owner_url": self._get_owner_url(response, script),
+                        "image_urls": self._get_image(script),
+                        "phone_number": self._get_phone_number(script),
+                    }
+                    return car_dict_update
+            except TypeError:
+                pass
+
+    def _get_owner_url(self, response, script):
+        if re.findall(self._re_pattern_dealer_check, script.css("::text").extract_first()):
+            owner_id = re.findall(self._re_pattern_user, script.css("::text").extract_first())
+            return response.urljoin(f"/user/{owner_id[0]}")
+        else:
+            owner_id = re.findall(self._re_pattern_dealer, script.css("::text").extract_first())
+            return urljoin(self.start_urls[0], owner_id[0].replace("%2F", "/"))
+
+    def _get_image(self, script) -> list:
+        img_list = re.findall(self._re_pattern_img, script.css("::text").extract_first())
+        return [
+            urljoin(self._base_image_url, url.replace("%2F", "/") + ".jpg") for url in img_list
+        ]
+
+    def _get_phone_number(self, script):
+        phone = unquote(
+            re.findall(self._re_pattern_phone, script.css("::text").extract_first())[0]
+        )
+        return b64decode(b64decode(phone)).decode()
+
+    def _save(self, data):
+        collection = self.db["cars"]
+        collection.insert_one(data)
